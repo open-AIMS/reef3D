b9e98229e90dca3583394fcce038c113edd4d64c
Manuel Gonzalez-Rivero <m.gonzalezrivero@aims.gov.au>
2018-10-11 10:27:27 +1000

    work in progress for batch processing images in photoscan

--- PhotoScan/python/BatchProcess.py
+++ PhotoScan/python/BatchProcess.py
@@ -1,11 +1,20 @@
-# Auto batch process for Agisoft Photoscan
-# @ H.GONG 2017/02/22
+##################################################################
+#### Batch process monitoring transects in Agisoft Photoscan #####
+##################################################################
+#Author: M. Gonzalez-Rivero
+#Date: Octuber 2018
+#Purpose:
 
 import PhotoScan
 import os,re,sys
+import pandas as pd
+from datetime import datetime
 
-# get the photo (.JPG) list in specified folder
+
 def getPhotoList(root_path, photoList):
+    '''
+    get the photo (.JPG) list in specified folder
+    '''
 	pattern = '.JPG$'
 	for root, dirs, files in os.walk(root_path):
 		for name in files:
@@ -13,8 +22,104 @@ def getPhotoList(root_path, photoList):
 				cur_path = os.path.join(root, name)
 				#print (cur_path)
 				photoList.append(cur_path)
+                
+
+def nearest(items, pivot):
+    '''
+    Search for the closest date in the scalebar file to the date images were collected
+    ''' 
+    items=items[items<pivot] #to consider only the dates previous to pivot
+    return min(items, key=lambda x: abs(x - pivot))
+                
+                
+def preProcess(chunk, scaletxt='scalebars.csv', qual=0.7,ttshld=60):
+    '''
+    Pre-processing the chunks by:
+    1) detecting markers
+    2) adding scale bars
+    3) filtering images by quality
+    4) Roller shutter compensation
+    
+    Inputs:
+    * doc: Photoscan document object(e.g., PhotoScan.app.document)
+    * scaletxt: name of the file containing the distance between markers and date of measurements (e.g., scalebars.csv)
+    * qual: quality threshold used to filter images
+    * ttshold:  tolerance threshold for marker detection
+    '''
+    
+    ### SET ENVIRONMENTAL VARIABLES 
+    docpath=doc.path
+    c=docpath.split('/projects')[0]
+    sbar = os.path.join(c,'reference_scales', scaletxt)
+
+    df=pd.read_csv(sbar,delimiter=',')
+    df['DATE']=pd.to_datetime(df['DATE'],format='%d/%m/%y')
+    
+    ############### Image quality control ###############################
+    print("---Estimating image quality---")
+    chunk.estimateImageQuality(chunk.cameras)
+    for camera in chunk.cameras:
+        if float(camera.meta["Image/Quality"]) < qual:
+            camera.enabled = False
+ 
+    ############### Marker detection ###############################
+    print("---Detecting Markers---")
+    chunk.detectMarkers(PhotoScan.TargetType.CircularTarget12bit, ttshld)
+    
+
+    ############### Add ScaleBars and distande from file ###############################
+    print("---Adding ScaleBars---")
+    camera = chunk.cameras[0]
+    #TODO modify this to allow multiple camera metadata. A posible to solution is to add the cruise date as an argument
+    #img_date=datetime.strptime(camera.photo.meta['Exif/DateTimeOriginal'], '%Y:%m:%d %H:%M:%S')
+    img_date=datetime.strptime('2018:10:07 10:45:23', '%Y:%m:%d %H:%M:%S')
+    ref_date=nearest(df['DATE'],img_date)
+    file = open(sbar, "rt")
+    markers = {}
+    for marker in chunk.markers:
+        markers[marker.label] = marker
+
+    lines = file.readlines()[1:]
+    for line in lines:
+        #FIXME there is a problem with the date format time data 'A' does not match format '%d/%m/%y' <MGR>
+        tdate,t1,t2, dist = datetime.strptime(line.split(',')[0],'%d/%m/%y'), line.split(',')[1],line.split(',')[2], line.split(',')[3]
+        if t1 in markers.keys() and t2 in markers.keys() and ref_date==tdate:
+            s=chunk.addScalebar(markers[t1], markers[t2])
+            s.reference.distance=float(dist)
+
+    file.close()
+        
+    ############### Enabling rolling shutter compensation ################################ 
+    print("---Rolling shutter compensation---")
+    
+    try:
+        for sensor in chunk.sensors:
+            rolling_shutter = True
+    except Exception as e:
+        print("Error:", e)
+        raise
+        
+#TODO move this to a separate script that wraps the prcessing per transect/reef <mgr>
+def runNetwork():
+    ''''
+    Run a task over the network
+    '''
+
+    client = PhotoScan.NetworkClient()
+
+    task1 = PhotoScan.NetworkTask()
+    task1.name = 'RunScript'
+    task1.params['path'] = "processing/script.py" #path to the script to be executed
+    task1.params['args'] = "argument1 argument2" #string of the arguments with space as separator
+
+    path = "projects/project.psx" 
+    client.connect('agisoft-qmgr.aims.gov.au') #server ip
+    batch_id = client.createBatch(path, [task1])
+    client.resumeBatch(batch_id)
+    print("Job started...")
+
 			
-def photoscanProcess(root_path):				
+def photoscanProcess(root_path, save_path, export_path, scaletxt):				
 	#PhotoScan.app.messageBox('hello world! \n')
 	PhotoScan.app.console.clear()
 
@@ -23,6 +128,7 @@ def photoscanProcess(root_path):
 
 	## save project
 	#doc.open("M:/Photoscan/practise.psx")
+    docname=
 	psxfile = root_path + 'practise.psx'
 	doc.save( psxfile )
 	print ('>> Saved to: ' + psxfile)
@@ -114,6 +220,9 @@ def photoscanProcess(root_path):
 	doc.save()
 
 # main
+qual = float(sys.argv[1]) # quality threshold for filtering images
+ttshld = int(sys.argv[2]) # Tolerance threshold for detecting markers
+
 folder = "M:/Photoscan/Photos/"
 photoscanProcess(folder)
 

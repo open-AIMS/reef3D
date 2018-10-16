0b10e5f6b43113b0898c5404a741e1f1090c709b
mgonzalezrivero <manuel.gonzalez.rivero@gmail.com>
2018-10-16 14:02:43 +1000

    testing data analyses in matlab

--- data_analyses/summary_transect.m
+++ data_analyses/summary_transect.m
@@ -1,56 +1,22 @@
 %%% Generate metrics of strucutral complex per transect
 
 %% LOAD PARAMETERS
-fpath='/Users/uqmgonz1/Dropbox/projects/3D_recruits/data/points/feather_v26';
+fpath='/net/138.7.48.21/3d_ltmp/exports/pointsXYZ/OM';
 f=dir(fpath);
 f = f(~[f.isdir]);
 [fPath, fName] = fileparts(fpath);
-qsizes=[0.1,0.2,0.5];
-pdens=50;
-addpath('/Users/uqmgonz1/Dropbox/3D_reconstructions/scripts/matlab_ACFR_QUT/')
-addpath('/Users/uqmgonz1/Dropbox/projects/3D_recruits/scripts')
-outpath=('/Users/uqmgonz1/Dropbox/projects/3D_recruits/data/structural_complexity');
+qsizes=[0.1,0.2];
+pdens=5;
+addpath('/export/home/l-p/mgonzale/Desktop/gits/reef3D/data_analyses/')
+addpath('/export/home/l-p/mgonzale/Desktop/gits/reef3D/MatToolbox')
+outpath=('/net/138.7.48.21/3d_ltmp/exports/structural_complexity/OM');
 reef=struct('reefname',fName, 'qsize',qsizes);
 core_info = evalc('feature(''numcores'')');
 %% set parallel workers
-% parpool(4)
+parpool(30)
 
 results=[];
 %% Calculate Structural complexity metrics per transect at different window sizes
-for i=(2:length(f))
-    results=[];
-    fname=fullfile(f(i).folder,f(i).name);
-    [tri,xyz,inPoints]=terrain_metrics(fname, pdens);
-    for x=(1:length(qsizes))
-        qs=qsizes(x);
-        [rgsty,slope, aspect, ~, rangez, sdevz, rgstyXY, ~,~, concavity, meandevz, ~, ~, ~] = trisurfterrainfeats(tri, xyz, qs, inPoints);
-        n=length(rgsty);
-        qsize=repmat(qs,n,1);
-        namevar=strsplit(f(i).name,'_');
-        reefname=repmat(namevar(1),n,1);
-        visit=repmat(sscanf(char(namevar(2)),'v%d'),n,1);
-        site=repmat(sscanf(char(namevar(3)),'s%d'),n,1);
-        transect=repmat(sscanf(char(namevar(4)),'t%d.txt'),n,1);
-        r=table(visit,reefname,site,transect,qsize,rgsty, slope, aspect, rangez, sdevz, rgstyXY, concavity, meandevz); 
-        %% PLOT RESULTS
-        trires=delaunay(inPoints(:, 1),inPoints(:,2));
-        h=figure('visible', 'off');
-        subplot(1,3,1), trisurf(tri,xyz(:,1),xyz(:,2),xyz(:,3)),...
-            shading interp, title('Bathymetry'), view(0,90),...
-            axis equal tight,  colorbar
-        subplot(1,3,2),...
-            trisurf(trires,inPoints(:, 1),...
-            inPoints(:,2),rgsty), shading interp, title('Rugosity'),...
-            view(0,90), axis equal tight, colorbar
-        
-        subplot(1,3,3),...
-            trisurf(trires,inPoints(:, 1),...
-            inPoints(:,2),rangez), shading interp, title('Range Heights (m)'),...
-            view(0,90), axis equal tight, colorbar
-        figname=strrep(char(f(i).name),'.txt',strcat('_q',num2str(qs,2),'.png'));
-        saveas(h,fullfile('/Users/uqmgonz1/Dropbox/projects/3D_recruits/fig/structural_complexity',figname))
-        close(h);
-        results=vertcat(results,r);
-    end
-    writetable(results,fullfile(outpath,f(i).name))
+parfor i=(2:4)
+    sc_wrapper(f(i),pdens,qsizes)
 end

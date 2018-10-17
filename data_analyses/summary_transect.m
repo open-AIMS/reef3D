28b6a4d81e0c8edd9e51ebde99724e30189074be
mgonzalezrivero <manuel.gonzalez.rivero@gmail.com>
2018-10-17 15:48:01 +1000

    modified PBs status number of cores and memory

--- data_analyses/summary_transect.m
+++ data_analyses/summary_transect.m
@@ -2,8 +2,8 @@
 
 %% PARAMETERS (to modify)
 fpath='/net/138.7.48.21/3d_ltmp/exports/pointsXYZ/OM'; % path where point data is located
-qsizes=[0.1]; % Window size vector for calculating sctructural complexity
-pdens=5; %Number of point per m2 on which to calculate structural complexity
+qsizes=[0.1,0.2,0.5]; % Window size vector for calculating sctructural complexity
+pdens=50; %Number of point per m2 on which to calculate structural complexity
 addpath('/export/home/l-p/mgonzale/Desktop/gits/reef3D/data_analyses/') %functions path
 addpath('/export/home/l-p/mgonzale/Desktop/gits/reef3D/MatToolbox') %function paths
 outpath=('/net/138.7.48.21/3d_ltmp/exports/'); %directoriy where files will be saved
@@ -15,9 +15,10 @@ f = f(~[f.isdir]);
 %reef=struct('reefname',fName, 'qsize',qsizes);
 %core_info = evalc('feature(''numcores'')');
 %% set parallel workers
-parpool(30)
+nc=feature('numcores');
+parpool(nc-2)
 
 %% Calculate Structural complexity metrics per transect at different window sizes
-parfor i=(2:4)
+parfor i=(2:length(f))
     sc_wrapper(f(i),pdens,qsizes, outpath, fName)
 end

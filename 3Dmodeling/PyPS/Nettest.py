a60125086ca8562cef44f5bb68b352c7652dd733
Manuel Gonzalez-Rivero <m.gonzalezrivero@aims.gov.au>
2018-10-24 12:19:27 +1000

    edits in Nettest

--- 3Dmodeling/PyPS/Nettest.py
+++ 3Dmodeling/PyPS/Nettest.py
@@ -5,7 +5,7 @@ import os
 doc=PhotoScan.app.document
 chunk=doc.chunk
 dp=doc.path
-sys.path.append(os.path.join(dp.split('testCamp')[0],'scripts/'))
+sys.path.append(os.path.join(dp.split('projects')[0],'scripts/'))
 from reef3D.PyToolbox import PSmodel as psm
 
 

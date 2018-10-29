a3af24c7875f21d515015f02756b58dede686210
Manuel Gonzalez-Rivero <m.gonzalezrivero@aims.gov.au>
2018-10-29 14:45:05 +1000

    modified summary_transects.m as a fucntion to take sys arguments

--- data_analyses/summary_transect.m
+++ data_analyses/summary_transect.m
@@ -1,17 +1,18 @@
+function summary_transect(fpath, qsizes,pdens,script_path,outpath)
 %%% Generate metrics of strucutral complex per transect
 
 %% PARAMETERS (to modify)
-fpath='/Users/uqmgonz1/Dropbox/projects/3D_recruits/data/OM/Feather/'; % path where point data is located
-qsizes=[0.1,0.2,0.5]; % Window size vector for calculating sctructural complexity
-pdens=0.2; %Number of point per m2 on which to calculate structural complexity
-addpath(genpath('/Users/uqmgonz1/Documents/GitHub/reef3D/')) %functions path
+%fpath='/Volumes/3d_ltmp/exports/pointsXYZ/OM/'; % path where point data is located
+%qsizes=[0.1,0.2,0.5]; % Window size vector for calculating sctructural complexity
+%pdens=0.2; %Number of point per m2 on which to calculate structural complexity
+addpath(genpath(script_path)) %functions path
 % addpath('/Users/uqmgonz1/Documents/GitHub/reef3D/MatToolbox') %function paths
-outpath=('/Users/uqmgonz1/Dropbox/projects/3D_recruits/'); %directoriy where files will be saved
+%outpath=('/Users/uqmgonz1/Dropbox/projects/3D_recruits/'); %directoriy where files will be saved
 
 %% EXTRA PARAMETERS
 f=dir(fpath);
 f = f(~[f.isdir]);
-[fPath, fName] = fileparts(fpath);
+[~ , fName] = fileparts(fpath);
 %reef=struct('reefname',fName, 'qsize',qsizes);
 %core_info = evalc('feature(''numcores'')');
 %% set parallel workers
@@ -19,8 +20,8 @@ f = f(~[f.isdir]);
 % mypool=parpool(40);
 
 %% Calculate Structural complexity metrics per transect at different window sizes
-for i=(2:4)%length(f))
-    tic
+for i=(2:length(f))
     sc_wrapper(f(i),pdens,qsizes, outpath, fName)
-    toc
+end
+
 end

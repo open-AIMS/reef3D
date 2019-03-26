98ab3d458522e919264339ef624133b4cd27f6ff
Manuel Gonzalez-Rivero <m.gonzalezrivero@aims.gov.au>
2019-03-26 13:55:35 +1000

    split videos using ffmpeg batch

--- PyToolbox/extract_frames.sh
+++ PyToolbox/extract_frames.sh
@@ -3,9 +3,11 @@
 
 cd $1
 
-for fname in *.*; do
+for fname in *; do
     for vname in $fname/*.MP4; do
-        ffmpeg -i "$fname/$vname" -qscale:v 2 -ss 6 -movflags use_metadata_tags -vf "fps=4" "${vname:0:2}"_"$fname"_V"${vname:9:2}"_%04d.jpg
+        if [-d "$fname"]; then
+            ffmpeg -i "$fname/$vname" -qscale:v 2 -ss 6 -movflags use_metadata_tags -vf "fps=4" "${vname:0:2}"_"$fname"_V"${vname:9:2}"_%04d.jpg
+        done
     done
 done 
 

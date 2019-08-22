c649eeeacb08dace7c7988845094b04087b6b14a
mgonzalezrivero <manuel.gonzalez.rivero@gmail.com>
2019-08-22 11:54:29 +1000

    minor edits in protocols

--- data_analyses/summary_transect.m
+++ data_analyses/summary_transect.m
@@ -4,8 +4,8 @@ function summary_transect(fpath,campose,script_path,outpath)
 %% PARAMETERS (to modify)
 %fpath='/Volumes/3d_ltmp/exports/pointsXYZ/OM/'; % path where point data is located
 %campose='/Volumes/3d_ltmp/exports/cameras/OM/'
-qsizes=[0.1,0.2,0.5]; % Window size vector for calculating sctructural complexity
-pdens=0.2; %Number of point per m2 on which to calculate structural complexity
+qsizes=[0.1,0.5]; % Window size vector for calculating sctructural complexity
+pdens=0.3; %Distance (m) between points on which to evaluate structural complexity
 addpath(genpath(script_path)) %functions path
 % addpath('/Users/uqmgonz1/Documents/GitHub/reef3D/MatToolbox') %function paths
 %outpath=('/Users/uqmgonz1/Dropbox/projects/3D_recruits/'); %directoriy where files will be saved
@@ -21,7 +21,11 @@ f = f(~[f.isdir]);
 
 %% Calculate Structural complexity metrics per transect at different window sizes
 for i=(1:length(f))
-    sc_wrapper(f(i),pdens,qsizes, outpath, campose)
+    namevar=strsplit(f(i).folder,'/');
+    %%% Ignore the transects already processed
+    if exist(fullfile(outpath,'structural_complexity', char(namevar(length(namevar))),f(i).name), 'file')==0
+        sc_wrapper(f(i),pdens,qsizes, outpath, campose)
+    end
 end
 
 end

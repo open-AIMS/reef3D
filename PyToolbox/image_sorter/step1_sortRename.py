efbb7dad5dcfbab59df9f367c838e8ab08ca2b04
Manuel Gonzalez-Rivero <manuel.gonzalez.rivero@gmail.com>
2020-04-15 16:34:32 +1000

    updated QRmap

--- PyToolbox/image_sorter/step1_sortRename.py
+++ PyToolbox/image_sorter/step1_sortRename.py
@@ -2,14 +2,14 @@
 ## STEP 1: Sort images and rename them based on QR codes
 from qrRename import  qrRename
 from sorter import sorter
-import sys
+import sys, os
 
 rp=str(sys.argv[1])
 ir=str(sys.argv[2])
 
 ##Set default values for some arguments that may not change often
 if len(sys.argv)==3:
-    qrm="C:/Users/mgonzale/Documents/gits/misc_manu/QRmap.csv"
+    qrm=os.path.join(os.getcwd(),"QRmap.csv")
     tg=3
 elif len(sys.argv)==4:
     qrm=str(sys.argv[3])

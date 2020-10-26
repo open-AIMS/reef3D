174c4b05ef5a2f939e8bbef01651c6386b23c6b8
Manuel Gonzalez-Rivero <manuel.gonzalez.rivero@gmail.com>
2020-10-26 17:08:02 +1000

    Project stats script

--- PyToolbox/Projstats.py
+++ PyToolbox/Projstats.py
@@ -1,6 +1,8 @@
 from glob import glob
-from reef3D.PyToolbox import PSeval as pe
+import os, sys
 sys.path.append('C:\\Users\\mgonzale\\Documents\\gits')
+from reef3D.PyToolbox import PSeval as pe
+
 
 projFolder='E:\\3d_ltmp\\projects'
 projList = [y for x in os.walk(projFolder) for y in glob(os.path.join(x[0], '*.psx'))]

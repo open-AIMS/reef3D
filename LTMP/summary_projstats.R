5f23b857006cbd3a99b22886b80ee385d69c0a8b
Manuel Gonzalez Rivero <M.GonzalezRivero@aims.gov.au>
2020-10-29 10:53:59 +1000

    minor fixes

--- LTMP/summary_projstats.R
+++ LTMP/summary_projstats.R
@@ -12,21 +12,24 @@ rap.nscale<-rap %>% subset(SCALE_ERROR=="NULL")
 df<-rbind(rap,rm)
 
 df<-df %>% mutate_if(is.character, list(~na_if(., "NULL")))%>%
-  mutate_at(vars(pALIGNED,SCALE_ERROR, MARKER_ERROR), as.numeric)
+  mutate_at(vars(pALIGNED,SCALE_ERROR, MARKER_ERROR), as.numeric)%>%
+  mutate(cal_year=as.numeric(substr(as.character(YEAR), 5,6))+2000)
 
 hist(df$pALIGNED)
 hist(df$SCALE_ERROR)
 hist(df$MARKER_ERROR)
 
 df.sum<-df%>%
-  group_by(YEAR,CAMPAIGN)%>%
+  group_by(cal_year,REEFNAME,CAMPAIGN)%>%
   mutate(outlier=if_else(MARKER_ERROR>5,1,0))%>%
-  summarise(COMPLETED=sum(STATUS==2),
-    pALIGNED=mean(pALIGNED, na.rm=T), 
-            SCALE_ERROR=median(SCALE_ERROR,na.rm=T),
-            MARKER_ERROR=median(MARKER_ERROR,na.rm=T),
+  summarise(DENSE_CLOUD=sum(STATUS=="2", na.rm=T),
+            ALIGNED=sum(STATUS=="1", na.rm=T),
+            DISABLED=sum(DISABLED=='yes', na.rm=T),
+            pALIGNED=mean(pALIGNED, na.rm=T), 
+            SCALE_ERROR=median(abs(SCALE_ERROR),na.rm=T),
+            MARKER_ERROR=median(abs(MARKER_ERROR),na.rm=T),
             NO_SCALED=sum(SCALED=="NO"),
-            NO_ALIGNED=sum(ALIGNED==0),
+            NO_ALIGNED=sum(is.na(pALIGNED)),
             OUTLIERS=sum(outlier, na.rm=T),
             total=length(REEFNAME))
 df.sum
